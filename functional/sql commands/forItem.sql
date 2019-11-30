c0d13d61b5b65020f1a5f300975d929bcb553cb3
Jeff Nguyen <jeffmiguel7@gmail.com>
2019-11-29 18:01:08 -0800

    For ease of use

---
+++ functional/sql commands/forItem.sql
@@ -0,0 +1,17 @@
+insert into item(itemID, itemName, brand, quantity, priority, notes)
+values
+(1, 'licorice', 'Red Vines', 2, 3, 'only the red ones'),
+(2, 'cookies', 'Oreos', 3, 5, 'for party'),
+(3, 'nacho cheese chips', 'Doritos', 1, 4, 'large bag please'),
+(4, 'white dress shirt', 'Stafford', 2, 3, 'something'),
+(5, 'blue tie', 'Kirkland', 2, 3, 'something'),
+(6, 'pants', 'Van Heusens', 1, 5, 'something'),
+(7, 'toothpaste', 'Colgate', 2, 3, 'something'),
+(8, 'towels', 'Pinzon', 2, 3, 'something'),
+(9, 'toothbrush', 'Oral-B', 2, 3, 'something'),
+(10, 'sweet tea', 'Pure Leaf', 1, 3, 'something'),
+(11, 'milk', 'Lucerne', 2, 5, 'something'),
+(12, 'orange juice', 'Tropicana', 1, 4, 'something'),
+(13, 'laptop', 'DELL', 1, 1, 'something'),
+(14, 'portable charger', 'Anker', 1, 2, 'something'),
+(15, 'earphones', 'Panasonic', 1, 1, 'something');
